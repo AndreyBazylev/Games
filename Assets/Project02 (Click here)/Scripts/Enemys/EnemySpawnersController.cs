9c571a30b13345d218bd8970766971f71788957d
AndreyBazylev <102666536+AndreyBazylev@users.noreply.github.com>
2022-07-07 12:41:30 +0300

    о_О

---
+++ Assets/Project02 (Click here)/Scripts/Enemys/EnemySpawnersController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnersController : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnersArray;
+    
+    [SerializeField] private bool _isSpawnEnable;
+    [SerializeField] private EnemyMover _enemyPrefab;
+
+    private readonly WaitForSeconds _wait = new WaitForSeconds(2);
+
+    private void Start()
+    {
+        StartCoroutine(ControlingEnemySpawning());
+    }
+
+    private IEnumerator ControlingEnemySpawning()
+    {
+        while (_isSpawnEnable)
+        {
+            for (int index = 0; index < _spawnersArray.Length; index++)
+            {
+                Debug.Log("UwU");
+                SpawnEnemy(_spawnersArray[index]);
+                yield return _wait;
+            }
+        }
+    }
+
+    private void SpawnEnemy(Transform spawnPosition)
+    {
+        Instantiate(_enemyPrefab, spawnPosition.transform.position, spawnPosition.transform.rotation);
+    }
+}
