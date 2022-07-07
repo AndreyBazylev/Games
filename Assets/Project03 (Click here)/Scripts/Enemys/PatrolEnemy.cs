a4732c6f99b12c01dbea28af0cdec30e9d10cd80
AndreyBazylev <102666536+AndreyBazylev@users.noreply.github.com>
2022-07-07 15:09:45 +0300

    Project03 (Click on folder whis this name)

---
+++ Assets/Project03 (Click here)/Scripts/Enemys/PatrolEnemy.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class PatrolEnemy : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    [SerializeField] bool _isEnemyPatroling;
+    [SerializeField] Transform _firstPoint;
+    [SerializeField] Transform _secondPoint;
+
+    private SpriteRenderer _sriteRenderer;
+
+    private Coroutine _targetCoruntine;
+
+    private void Start()
+    {
+        _sriteRenderer = GetComponent<SpriteRenderer>();
+        transform.position = _firstPoint.position;
+
+        StartCoroutine(ChangeDirection());
+    }
+
+    private IEnumerator ChangeDirection()
+    {
+        while (_isEnemyPatroling)
+        {
+            if (transform.position == _firstPoint.position)
+            {
+                _sriteRenderer.flipX = false;
+
+                if (_targetCoruntine == null)
+                {
+                    _targetCoruntine = StartCoroutine(GoToTarget(_secondPoint));
+                }
+
+                else
+                {
+                    StopCoroutine(_targetCoruntine);
+                    _targetCoruntine = StartCoroutine(GoToTarget(_secondPoint));
+                }
+            }
+
+            else if (transform.position == _secondPoint.position)
+            {
+                _sriteRenderer.flipX = true;
+
+                if (_targetCoruntine == null)
+                {
+                    _targetCoruntine = StartCoroutine(GoToTarget(_firstPoint));
+                }
+
+                else
+                {
+                    StopCoroutine(_targetCoruntine);
+                    _targetCoruntine = StartCoroutine(GoToTarget(_firstPoint));
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator GoToTarget(Transform target)
+    {
+        Debug.Log("uwu");
+
+        while (transform.position != target.position)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Project03 (Click here)/Scripts/Money/Wallet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Wallet : MonoBehaviour
+{
+    [SerializeField] int _money;
+
+    public void IcreaseMoney()
+    {
+        _money++;
+    }
+}
