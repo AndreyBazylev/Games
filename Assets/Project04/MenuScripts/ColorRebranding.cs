07033ac07b7feac6bf50e327b2f7f3a91995ca42
AndreyBazylev <102666536+AndreyBazylev@users.noreply.github.com>
2022-07-12 15:25:43 +0300

    Project05 (Click on folder with this name)

---
+++ Assets/Project04/MenuScripts/ColorRebranding.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorRebranding : MonoBehaviour
+{
+    [SerializeField] private Image _panel;
+    [SerializeField] private Color _targetColor;
+
+    private float _speed = 0.1f;
+    private bool _isIncrease = true;
+    private Color _startColor;
+    private Coroutine _colorCoruntine;
+
+    private void Start()
+    {
+        _startColor = _panel.color;
+    }
+
+    private void Update()
+    {
+        if (!_isIncrease)
+        {
+            if (_colorCoruntine == null)
+            {
+                _colorCoruntine = StartCoroutine(ChangeColor(_targetColor));
+            }
+
+            else
+            {
+                StopCoroutine(_colorCoruntine);
+                _colorCoruntine = StartCoroutine(ChangeColor(_targetColor));
+            }
+        }
+
+        else
+        {
+            if (_colorCoruntine == null)
+            {
+                _colorCoruntine = StartCoroutine(ChangeColor(_startColor));
+            }
+
+            else
+            {
+                StopCoroutine(_colorCoruntine);
+                _colorCoruntine = StartCoroutine(ChangeColor(_startColor));
+            }
+        }
+    }
+
+    private IEnumerator ChangeColor(Color target)
+    {
+        Debug.Log("uwu");
+        _isIncrease = !_isIncrease; 
+
+        while (_panel.color != target)
+        {
+            
+            _panel.color = Color.Lerp(_panel.color, _targetColor, _speed);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Project05(Click here)/Scripts/HealthBar.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _healthBarSlider;
+    [SerializeField] private Text _healthBarText;
+    [SerializeField] private float _healthChangeSpeed;
+    [SerializeField] private float _chageHealthSize;
+
+    private float _health = 100;
+    private float _healthBar;
+    private Coroutine _currentCorountine;
+
+    private void Start()
+    {
+        _healthBar = _health;
+    }
+
+    public void IncreaseDeltaHealth()
+    {
+        _health += _chageHealthSize;
+        RestartHeathCorountine();
+    }
+
+    public void DecreaseDeltaHealth()
+    {
+        _health -= _chageHealthSize;
+        RestartHeathCorountine();
+    }
+
+    private void RestartHeathCorountine()
+    {
+        if (_currentCorountine == null)
+        {
+            _currentCorountine = StartCoroutine(ChangeHealth());
+        }
+
+        else
+        {
+            StopCoroutine(_currentCorountine);
+            _currentCorountine = StartCoroutine(ChangeHealth());
+        }
+    }
+
+    private void PrintHealth(float healthBar)
+    {
+        _healthBarText.text = Convert.ToString(Convert.ToInt32(healthBar)) + '%';
+        _healthBarSlider.value = healthBar;
+    }
+
+    private IEnumerator ChangeHealth()
+    {
+        while (_healthBar != _health)
+        {
+            _healthBar = Mathf.MoveTowards(_healthBar, _health, _healthChangeSpeed);
+            PrintHealth(_healthBar);
+            yield return null;
+        }
+    }
+}
