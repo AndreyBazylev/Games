54572d14e94aa712cd258f4084ce0c1fef352d9d
AndreyBazylev <102666536+AndreyBazylev@users.noreply.github.com>
2022-07-13 11:13:14 +0300

    refactoring0.1

---
+++ Assets/Project05(Click here)/Scripts/Bar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Bar : MonoBehaviour
+{
+    [SerializeField] private Slider _barSlider;
+    [SerializeField] private Text _barText;
+
+    private float _value;
+
+    public void SetValue(float newValue)
+    {
+        _value = newValue;
+        PrintValue();
+    }
+
+    private void PrintValue()
+    {
+        _barText.text = Convert.ToString(Convert.ToInt32(_value)) + '%';
+        _barSlider.value = _value;
+    }
+
+    
+}
---
+++ Assets/Project05(Click here)/Scripts/Health.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private Bar _healthBarOdject;
+    [SerializeField] private float _healthChangeSpeed;
+    [SerializeField] private float _chageHealthSize;
+
+    private float _health = 100;
+    private float _healthBar;
+    private Coroutine _currentCorountine;
+
+    private void Start()
+    {
+        _healthBar = _health;
+    }
+
+    private void Update()
+    {
+        _healthBarOdject.SetValue(_healthBar);
+    }
+
+    public void IncreaseDeltaHealth()
+    {
+        _health += _chageHealthSize;
+        RestartHeathCorountine();
+    }
+
+    public void DecreaseDeltaHealth()
+    {
+        _health -= _chageHealthSize;
+        RestartHeathCorountine();
+    }
+
+    private void RestartHeathCorountine()
+    {
+        if (_currentCorountine == null)
+        {
+            _currentCorountine = StartCoroutine(ChangeHealth());
+        }
+
+        else
+        {
+            StopCoroutine(_currentCorountine);
+            _currentCorountine = StartCoroutine(ChangeHealth());
+        }
+    }
+
+    private IEnumerator ChangeHealth()
+    {
+        while (_healthBar != _health)
+        {
+            _healthBar = Mathf.MoveTowards(_healthBar, _health, _healthChangeSpeed);
+            yield return null;
+        }
+    }
+}
